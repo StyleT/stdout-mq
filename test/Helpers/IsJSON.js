3d6d65d8d67b7c216270c67855c735803b501d9d
Oleh Momot <Oleh_Momot@pm.me>
2020-02-25 17:33:52 +0200

    Cover isJSON with tests

---
+++ test/Helpers/IsJSON.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const expect = require('chai').expect;
+const helpersLib = require('../../lib/Helpers');
+
+describe('IsJSON', () => {
+  it('Should return true when a provided string has JSON format', () => {
+    expect(helpersLib.isJSON('{"array": ["string", {"number": 10}]}')).to.be.equal(true);
+  });
+
+  it('Should return false when a provided string does not have JSON format', () => {
+    expect(helpersLib.isJSON('  object: {"array": ["string", {"number": 10}]}   ')).to.be.equal(false);
+  });
+});
