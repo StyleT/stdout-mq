fc03b2375fea0018b41aef7767ea21c697d44541
Oleh Momot <Oleh_Momot@pm.me>
2020-02-25 12:18:02 +0200

    Update tests due to updated dependencies

--- test/RabbitMQ/Initialization.js
+++ test/RabbitMQ/Initialization.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const expect = require('@itavy/test-utilities').getExpect();
+const expect = require('chai').expect;
 const { RabbitMQTransport } = require('../../lib/RabbitMQTransport');
 const fixtures = require('./Fixtures');
 
